7705fd88443280e0a834f8aae63751cecc78238d
Uday <udaybhisikar@gmail.com>
2026-08-01 14:03:37 +0530

    Add persistent dark and light mode

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,5 +1,24 @@
 const themeBtn = document.querySelector(".theme-btn");
 
+// Page load hone par saved theme check karo
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+    themeBtn.textContent = "☀️";
+} else {
+    themeBtn.textContent = "🌙";
+}
+
+// Theme button click
 themeBtn.addEventListener("click", () => {
+
     document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        localStorage.setItem("theme", "dark");
+        themeBtn.textContent = "☀️";
+    } else {
+        localStorage.setItem("theme", "light");
+        themeBtn.textContent = "🌙";
+    }
+
 });
